bccebd2019fc0f04cccb8d126463db04bb8ec90e
Dysthesis <antheoraviel@protonmail.com>
2026-01-07 10:46:05 +1100

    I beg please work

--- after/queries/rust/highlights.scm
+++ after/queries/rust/highlights.scm
@@ -1,4 +1,8 @@
 ;; extends
 
-([ "return" "yield" "?" ] @keyword.return (#set! priority 110))
-([ "break" "continue" ] @keyword.exception (#set! priority 110))
+([ 
+   "return"
+   "yield"
+   "break"
+   "continue"
+ ] @keyword.return (#set! priority 110))
