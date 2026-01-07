2d60173f283fa7c71ff575bd8090f3e55a1af524
Dysthesis <antheoraviel@protonmail.com>
2026-01-07 12:24:14 +1100

    WIP

--- after/queries/c/highlights.scm
+++ after/queries/c/highlights.scm
@@ -1,32 +1,8 @@
 ;; extends
 
 ([
-  "continue"
+  "return"
   "break"
-  "goto"
-] @keyword.return (#set! priority 300))
-
-("break" @keyword.return
-  (#has-ancestor? @keyword.return break_statement)
-  (#set! priority 300))
-
-("continue" @keyword.return
-  (#has-ancestor? @keyword.return continue_statement)
-  (#set! priority 300))
-
-("goto" @keyword.return
-  (#has-ancestor? @keyword.return goto_statement)
-  (#set! priority 300))
-
-("&&" @keyword.return
-  (#has-ancestor? @keyword.return binary_expression)
-  (#set! priority 300))
-
-("||" @keyword.return
-  (#has-ancestor? @keyword.return binary_expression)
-  (#set! priority 300))
-
-(call_expression
-  function: (identifier) @keyword.return
-  (#match? @keyword.return "^(unreachable|__builtin_unreachable|__builtin_trap|__assume)$")
-  (#set! priority 300))
+  "continue"
+] @keyword.return
+(#set! "priority" 110))
