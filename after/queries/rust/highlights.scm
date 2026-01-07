c7f22355baab67550c716bc0ff328b445d471e5e
Dysthesis <antheoraviel@protonmail.com>
2026-01-07 11:13:03 +1100

    Still trying to get rust treesitter to work

--- after/queries/rust/highlights.scm
+++ after/queries/rust/highlights.scm
@@ -1,9 +1,23 @@
 ;; extends
 
-([ 
-   "return"
-   "yield"
-   "break"
-   "continue"
- ] @keyword.return 
- (#set! "priority" 110))
+;; Control-flow interruption and short-circuiting.
+([
+  "return"
+  "break"
+  "continue"
+  "?"
+  "yield"
+] @keyword.return
+(#set! "priority" 110))
+
+;; Async-style control-flow.
+([
+  "await"
+] @keyword.return
+(#set! "priority" 110))
+
+;; "unreachable" in Rust is not a keyword; it is typically the macro `unreachable!()`.
+((macro_invocation
+  macro: (identifier) @keyword.return
+  (#eq? @keyword.return "unreachable"))
+(#set! "priority" 110))
