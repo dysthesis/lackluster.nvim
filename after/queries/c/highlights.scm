9de8198ba370b8330516fc3170d45154d5dccc1e
Dysthesis <antheoraviel@protonmail.com>
2026-01-07 11:44:06 +1100

    Try to fix C highlights

--- after/queries/c/highlights.scm
+++ after/queries/c/highlights.scm
@@ -1,27 +1,43 @@
 ;; extends
 
-;; Control-flow interruption.
-([
-  "return"
-  "break"
-  "continue"
-  "goto"
-] @keyword.return
-(#set! "priority" 200))
+(return_statement "return" @keyword.return
+  (#set! "priority" 200))
 
-;; Short-circuit operators.
+(break_statement "break" @keyword.return
+  (#set! "priority" 200))
+
+(continue_statement "continue" @keyword.return
+  (#set! "priority" 200))
+
+(goto_statement "goto" @keyword.return
+  (#set! "priority" 200))
+
+(return_statement) @keyword.return
+  (#set! "priority" 50)
+
+(break_statement) @keyword.return
+  (#set! "priority" 50)
+
+(continue_statement) @keyword.return
+  (#set! "priority" 50)
+
+(goto_statement) @keyword.return
+  (#set! "priority" 50)
+
 (binary_expression
   operator: ["&&" "||"] @keyword.return
   (#set! "priority" 200))
 
-;; Ternary conditional operator tokens.
+(binary_expression
+  ["&&" "||"] @keyword.return
+  (#set! "priority" 200))
+
 (conditional_expression
   "?" @keyword.return
   ":" @keyword.return
   (#set! "priority" 200))
 
-;; Common "unreachable" spellings in real-world C codebases.
-((call_expression
+(call_expression
   function: (identifier) @keyword.return
-  (#match? @keyword.return "^(__builtin_unreachable|unreachable|__assume)$"))
-(#set! "priority" 200))
+  (#match? @keyword.return "^(__builtin_unreachable|__builtin_trap|__assume)$")
+  (#set! "priority" 200))
