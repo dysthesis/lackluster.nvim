ed435ca22839a8397cd0b68000d8f99161596635
Dysthesis <antheoraviel@protonmail.com>
2026-01-07 12:18:38 +1100

    WIP

--- after/queries/c/highlights.scm
+++ after/queries/c/highlights.scm
@@ -1,20 +1,26 @@
 ;; extends
 
-([
-  "break"
-  "continue"
-  "goto"
-] @keyword.return
-(#set! priority 300))
+("break" @keyword.return
+  (#has-ancestor? @keyword.return break_statement)
+  (#set! priority 300))
 
-(["&&" "||"] @keyword.return
- (#set! priority 300))
+("continue" @keyword.return
+  (#has-ancestor? @keyword.return continue_statement)
+  (#set! priority 300))
 
-(conditional_expression
-  ["?" ":"] @keyword.return
+("goto" @keyword.return
+  (#has-ancestor? @keyword.return goto_statement)
+  (#set! priority 300))
+
+("&&" @keyword.return
+  (#has-ancestor? @keyword.return binary_expression)
+  (#set! priority 300))
+
+("||" @keyword.return
+  (#has-ancestor? @keyword.return binary_expression)
   (#set! priority 300))
 
 (call_expression
   function: (identifier) @keyword.return
-  (#match? @keyword.return "^(__builtin_unreachable|__builtin_trap|__assume)$")
+  (#match? @keyword.return "^(unreachable|__builtin_unreachable|__builtin_trap|__assume)$")
   (#set! priority 300))
