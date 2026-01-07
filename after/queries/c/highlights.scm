ca9bcb1ca063a5c13cd2ee11877071f32da9b417
Dysthesis <antheoraviel@protonmail.com>
2026-01-07 12:00:18 +1100

    WIP

--- after/queries/c/highlights.scm
+++ after/queries/c/highlights.scm
@@ -5,46 +5,16 @@
   "continue"
   "goto"
 ] @keyword.return
-(#set! "priority" 200))
+(#set! priority 300))
 
-(return_statement "return" @keyword.return
-  (#set! "priority" 200))
-
-(break_statement "break" @keyword.return
-  (#set! "priority" 200))
-
-(continue_statement "continue" @keyword.return
-  (#set! "priority" 200))
-
-(goto_statement "goto" @keyword.return
-  (#set! "priority" 200))
-
-(return_statement) @keyword.return
-  (#set! "priority" 50)
-
-(break_statement) @keyword.return
-  (#set! "priority" 50)
-
-(continue_statement) @keyword.return
-  (#set! "priority" 50)
-
-(goto_statement) @keyword.return
-  (#set! "priority" 50)
-
-(binary_expression
-  operator: ["&&" "||"] @keyword.return
-  (#set! "priority" 200))
-
-(binary_expression
-  ["&&" "||"] @keyword.return
-  (#set! "priority" 200))
+(["&&" "||"] @keyword.return
+ (#set! priority 300))
 
 (conditional_expression
-  "?" @keyword.return
-  ":" @keyword.return
-  (#set! "priority" 200))
+  ["?" ":"] @keyword.return
+  (#set! priority 300))
 
 (call_expression
   function: (identifier) @keyword.return
   (#match? @keyword.return "^(__builtin_unreachable|__builtin_trap|__assume)$")
-  (#set! "priority" 200))
+  (#set! priority 300))
